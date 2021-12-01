99b91b6dbccdf37243a3e5950493e384facf1807
unknown <vi.xedy@gmail.com>
2021-12-01 15:30:28 -0300

    finaliza e anuncia vencedor

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var vez=1
-var vencedor=""
+var vez = 1
+var vencedor= ""
+
 $(document).ready(function(){
   $(".casa").click(function(){
   var fundo = $(this).css("background-image");
-if(fundo == "none"||fundo == ""){
-var fig = "url("+vez.toString()+".jpg)"
+if(fundo == "none" || fundo == ""){
+var fig = "url(" + vez.toString() + ".jpg)"
 $(this).css("background",fig);
 vez = (vez == 1 ? 2:1);
 verificarFimDeJogo();
 
-}});});
-function verificarFimDeJogo(){
+}
+
+ function verificarFimDeJogo(){
+
+  var existemCasasIguais = [
+    verificaSeTemCasasIguais(1,2,3),
+    verificaSeTemCasasIguais(4,5,6,),
+     verificaSeTemCasasIguais(7,8,9),
+     verificaSeTemCasasIguais(1,4,7),
+      verificaSeTemCasasIguais(2,5,8),
+       verificaSeTemCasasIguais(3,9,6),
+       verificaSeTemCasasIguais(1,5,9),
+        verificaSeTemCasasIguais(3,5,7)];
+
+  if(existemCasasIguais.indexOf(true) >= 0){
+  $("#resultado").html("<h1> O jogador " + vencedor + " venceu!</h1>");
+  $(".casa").off("click");
+  }
 }
 
 function verificaSeTemCasasIguais(numeroPrimeiraCasa, numeroSegundaCasa, numeroTerceiraCasa){
@@ -24,6 +41,15 @@ var terceiraCasa = $("#casa" + numeroTerceiraCasa);
 
 if ((fundoPrimeiraCasa == fundoSegundaCasa) && (fundoTerceiraCasa == fundoSegundaCasa)  && (fundoPrimeiraCasa != "none" && fundoPrimeiraCasa != "")){
 
-} else{
-  return False;
+if(fundoPrimeiraCasa.indexOf("1.jpg") >= 0){
+  vencedor = "1";
+  return true;
+}else{
+  vencedor = "2";
+  return true;
+}}else{
+  return false;
 }
+}
+});
+});
